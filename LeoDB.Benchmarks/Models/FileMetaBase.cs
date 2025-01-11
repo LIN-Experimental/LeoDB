fd321bb44de19355b110c203a9e51ce0012d0d59
Alexander Giraldo <giraldojhong4@gmail.com>
2025-01-11 17:12:42 -0500

    Correción de errores

--- LeoDB.Benchmarks/Models/FileMetaBase.cs
+++ LeoDB.Benchmarks/Models/FileMetaBase.cs
@@ -1,33 +1,32 @@
 using System;
 
-namespace LeoDB.Benchmarks.Models
+namespace LeoDB.Benchmarks.Models;
+
+public class FileMetaBase
 {
-    public class FileMetaBase
-    {
-        [CollectionFieldIgnore]
-        public const string BsonIdPropertyKey = "_id";
+    [CollectionFieldIgnore]
+    public const string BsonIdPropertyKey = "_id";
 
-        [CollectionId]
-        public virtual string Id => $"{FileId}_{Version}";
+    [CollectionId]
+    public virtual string Id => $"{FileId}_{Version}";
 
-        public Guid FileId { get; set; }
+    public Guid FileId { get; set; }
 
-        public string ParentId { get; set; }
+    public string ParentId { get; set; }
 
-        public string Title { get; set; }
+    public string Title { get; set; }
 
-        public string MimeType { get; set; }
+    public string MimeType { get; set; }
 
-        public int Version { get; set; }
+    public int Version { get; set; }
 
-        public DateTimeOffset? ValidFrom { get; set; }
+    public DateTimeOffset? ValidFrom { get; set; }
 
-        public DateTimeOffset? ValidTo { get; set; }
+    public DateTimeOffset? ValidTo { get; set; }
 
-        public bool IsFavorite { get; set; }
+    public bool IsFavorite { get; set; }
 
-        public bool ShouldBeShown { get; set; }
+    public bool ShouldBeShown { get; set; }
 
-        public virtual bool IsValid => ValidFrom == null || ValidFrom <= DateTimeOffset.UtcNow && ValidTo == null || ValidTo > DateTimeOffset.UtcNow;
-    }
+    public virtual bool IsValid => ValidFrom == null || ValidFrom <= DateTimeOffset.UtcNow && ValidTo == null || ValidTo > DateTimeOffset.UtcNow;
 }
--- LeoDB/Client/Mapper/Attributes/CollectionFieldIgnoreAttribute.cs
+++ LeoDB/Client/Mapper/Attributes/CollectionFieldIgnoreAttribute.cs
@@ -5,7 +5,7 @@ namespace LeoDB;
 /// <summary>
 /// Indicar que la propiedad no se persistirá en la serialización.
 /// </summary>
-[AttributeUsage(AttributeTargets.Property)]
+[AttributeUsage(AttributeTargets.Property | AttributeTargets.Field)]
 public class CollectionFieldIgnoreAttribute : Attribute
 {
 }
